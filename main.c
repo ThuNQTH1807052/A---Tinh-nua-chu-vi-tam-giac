ee441dfa323921410926405cbe57c4dda162612f
quynhthu <thunqth1807052@fpt.edu.vn>
2018-10-10 19:10:46 +0700

    commit lan 2

--- main.c
+++ main.c
@@ -1,23 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int tinhNuaChuViTamGiac(int a, int b, int c){
+float tinhNuaChuViTamGiac(int a, int b, int c){
     return (a + b + c)/2;
 }
 int main() {
 
     int a, b, c;
-    printf("Nhap so do canh thu nhat:\n");
+    printf("Nhap so do canh thu nhat:");
     scanf("%d", &a);
-    printf("Nhap so do canh thu hai:\n");
+    printf("Nhap so do canh thu hai:");
     scanf("%d", &b);
-    printf("Nhap so do canh thu ba:\n");
+    printf("Nhap so do canh thu ba:");
     scanf("%d", &c);
-    if (a <= 0, b <= 0, c <= 0){
+    if (a, b, c != 0){
+        if ((a+b>c)|| (a+c>b)||(b+c>a)) {
+            printf("Nua chu vi cua tam giac la %2.f", tinhNuaChuViTamGiac(a, b, c));
+        }
+        else {
+            printf("Tong hai canh cua mot tam giac luon phai lon hon canh con lai.\n");
+            }
+    }
+    else{
         printf("Do dai mot canh tam giac khong the nho hon hoac bang 0.\n");
     }
-
-
     return 0;
-
 }
