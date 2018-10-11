360a81f5cb71b82d8ca069ce5b64481a9bb84031
quynhthu <thunqth1807052@fpt.edu.vn>
2018-10-11 18:44:30 +0700

    commit them 1 lan nua

--- main.c
+++ main.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-float tinhNuaChuViTamGiac(int a, int b, int c){
-    return (a + b + c)/2;
+float tinhNuaChuViTamGiac(float a, float  b, float c){
+    return (a + b + c) / 2;
 }
 int main() {
 
-    int a, b, c;
-    printf("Nhap so do canh thu nhat:");
-    scanf("%d", &a);
-    printf("Nhap so do canh thu hai:");
-    scanf("%d", &b);
-    printf("Nhap so do canh thu ba:");
-    scanf("%d", &c);
+    float a, b, c;
+    printf("Nhap so do canh thu nhat: ");
+    scanf("%f", &a);
+    printf("Nhap so do canh thu hai: ");
+    scanf("%f", &b);
+    printf("Nhap so do canh thu ba: ");
+    scanf("%f", &c);
     if (a > 0 && b > 0 && c > 0){
-        if ((a+b>c)|| (a+c>b)||(b+c>a)) {
-            printf("Nua chu vi cua tam giac la %2.f", tinhNuaChuViTamGiac(a, b, c));
+        if ((a+b>c) && (a+c>b) && (b+c>a)) {
+            printf("Nua chu vi cua tam giac la %.2f", tinhNuaChuViTamGiac(a, b, c));
         }
         else {
             printf("Tong hai canh cua mot tam giac luon phai lon hon canh con lai.\n");
